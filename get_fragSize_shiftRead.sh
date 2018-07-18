1f7ac0572a260d03c0aff7be55cb1c6cc1ae1f89
jingkui <jingkui.wang@imp.ac.at>
2018-07-18 13:44:43 +0200

    minor change for get_fragSize_shiftRead.sh

--- get_fragSize_shiftRead.sh
+++ get_fragSize_shiftRead.sh
@@ -6,8 +6,9 @@ nb_cores=6
 cwd=`pwd`
 Genome="/groups/bell/jiwang/Genomes/C_elegans/ce11/ce11_sequence/ce11.chrom.sizes"
 
-DIR_Input="${cwd}/alignments/BAMs_unique_rmdup"
+DIR_Input="${cwd}/alignments/BAMs_All"
 DIR_fragSize="${cwd}/QCs/fragSize_distribution"
+
 DIR_shift="${cwd}/alignments/BAMs_unique_rmdup_shift"
 DIR_logs="${DIR_fragSize}/logs"
 
