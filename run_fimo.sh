0e1b95a65a9dbb87870024b0657372f16275eb5a
jingkui.wang <jingkui.wang@imp.ac.at>
2021-05-19 06:53:56 +0200

    try to update new SwissRegulon hg

--- run_fimo.sh
+++ run_fimo.sh
@@ -14,12 +14,14 @@ ml load meme/5.1.1-foss-2018b-python-3.6.6
 ml load bedtools/2.27.1-foss-2018b
 
 mkdir -p FIMO
+
 bedtools sort -i peaks/peaks_for_fimo.bed > FIMO/peaks_for_fimo_sorted.bed
 
 bedtools getfasta -fi /groups/tanaka/People/current/jiwang/Genomes/axolotl/AmexG_v6.DD.corrected.round2.chr.fa \
 	 -bed FIMO/peaks_for_fimo_sorted.bed > FIMO/peaks_for_fimo.fa 
+
 fimo --thresh 0.0001 \
      --oc FIMO/fimo_out \
-     /groups/tanaka/People/current/jiwang/Databases/motifs_TFs/PWMs_Mus/motifMatrix_SuissRegulon.meme \
+     /groups/tanaka/People/current/jiwang/Databases/motifs_TFs/SwissRegulon_PWMs/hg19_weight_matrices_v2.meme \
      FIMO/peaks_for_fimo.fa
 
