56e7a0bdeb23ff9169cab3077e26bdc06a20b883
jingkui.wang <jingkui.wang@imp.ac.at>
2021-09-09 12:18:43 +0200

    update featureCounts_readPeaks.sh by adding arguments

--- featureCounts_readPeaks.sh
+++ featureCounts_readPeaks.sh
@@ -3,17 +3,46 @@
 # Here teh gtf files used has all annotation including non-coding regions and miRNAs
 # because we want to counts reads not only for gene features but also biotypes (e.g. protein coding genes and miRNAs)
 ####################
-nb_cores=8
+while getopts ":hD:p:" opts; do
+    case "$opts" in
+        "h")
+            echo "script to quantify read count from aligned bam files)"
+            echo "Two arguments required"
+            echo "-D the directory of bam files "
+            echo "-p the peak file of format saf for feature count "
+            echo "Usage:"
+            echo "$0 -D alignments/BAMs_All -p peaks_macs2_merged.saf"
+            exit 0
+            ;;
+        "D")
+            DIR_input="$OPTARG";
+            ;;
+        "p")
+            SAF="$OPTARG";
+            ;;
+        "?")
+            echo "Unknown option $opts"
+            ;;
+        ":")
+            echo "No argument value for option $opts"
+            ;;
+        esac
+done
+
+nb_cores=16
 jobName='featurecounts'
 format=bam
 strandSpec=0;
 cutoff_quality=30
-SAF="/groups/tanaka/People/current/jiwang/projects/positional_memory/Data/R10723_atac/calledPeaks_pval.0.001/merge_peak.saf"
 
-DIR=`pwd`
-DIR_input="${PWD}/bams_used"
-DIR_output="${DIR}/featurecounts.Q${cutoff_quality}"
+#SAF="/groups/tanaka/People/current/jiwang/projects/positional_memory/Data/R10723_atac/calledPeaks_pval.0.001/merge_peak.saf"
+
+#DIR=`pwd`
+#DIR_input="${PWD}/bams_used"
+
+DIR_output="${PWD}/featurecounts_peaks.Q${cutoff_quality}"
 dir_logs=$PWD/logs
+
 echo $DIR_input
 echo $DIR_output
 
@@ -33,7 +62,7 @@ do
 #!/usr/bin/bash
 
 #SBATCH --cpus-per-task=$nb_cores
-#SBATCH --time=240
+#SBATCH --time=480
 #SBATCH --mem=16G
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
